451bf0a683a1fe1013fba4128e2f87366913b82f
Makee Ham <ina0312@naver.com>
2025-01-04 22:59:34 +0900

    250104

---
+++ intro06SelectorExercise/script.js
@@ -0,0 +1,6 @@
+//$(document).ready(function () {
+$("div").css("background-color", "purple"); // 이건 css 문법대로 써줌
+$("div.highlight").width("200px");
+$("#third").css({ border: "2px solid orange" }); // 이렇게 하면 js대로 property 써줌
+$("div:first-of-type").css("color", "pink");
+//});
---
+++ intro07TextAndHTML/script.js
@@ -0,0 +1,22 @@
+// .text() - corresponds to textContent property
+/*
+콘솔로 실습해보자
+
+$("ul").text();
+
+$("li").text();
+
+$("h1").text("New Text!!!");
+
+$("li").text("Rusty, Colt's dog, is adorable");
+
+*/
+
+// .html() - corresponds to inner html
+/*
+$('ul').html();
+
+$('ul').html("<li>I Hacked your UL!</li><li>Rusty is still adorable!</li>");
+
+$('li').html("<a href='google.com'>CLICK ME TO GO TO GOOGLE</a>");
+*/
